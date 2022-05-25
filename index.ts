b8a217bc81fa70530ddcb4becd510ae7f2fc079a
wmshoon <97453378+wmshoon@users.noreply.github.com>
2022-05-25 17:11:16 +0900

    change to TS

---
+++ index.ts
@@ -0,0 +1,10 @@
+const str: string = "happy CODEing";
+
+class Coffee {
+    private name: string;
+    private temperature: string;
+    constructor(name: string, temperature: string) {
+        this.name = name;
+        this.temperature = temperature;
+    }
+}
