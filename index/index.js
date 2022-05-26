54069205dd6126af1ab92b6dd54ed5016813e8ef
wmshoon <97453378+wmshoon@users.noreply.github.com>
2022-05-27 00:04:30 +0900

    조지긴 했는데 됨

--- index/index.js
+++ index/index.js
@@ -1,15 +1,64 @@
-var colorChangeBtn = document.querySelector("#themeChangeBtn");
-var otherElements = document.querySelector("#mobgrid");
-var defaultTheme = "dark";
+// const otherElements: any = document.querySelector("#mobgrid");
+// const defaultTheme: string = "dark";
+// colorChangeBtn.addEventListener("click", clicked);
+// function clicked() {
+//     if (dark == true) {
+//         document.body.style.backgroundColor = "rgb(57, 62, 70)";
+//         dark = false;
+//     } else if (dark == false) {
+//         dark = true;
+//         document.body.style.backgroundColor = "rgb(198, 193, 185)";
+//     }
+// }
 var dark = true;
-colorChangeBtn.addEventListener("click", clicked);
-function clicked() {
+var themeChangeBtn = document.querySelector("#themeChangeBtn");
+var navbar = document.querySelector("#navbar");
+var after_navbar = document.querySelector("#after_navbar");
+var realtime_best = document.querySelector("#realtime_best");
+var todays_hot = document.querySelector("#todays_hot");
+var login_container = document.querySelector("#login_container");
+var mobgrid = document.querySelector("#mobgrid");
+backgroundDark();
+backgroundLight();
+navbar.classList.add("navbar_dark");
+after_navbar.classList.add("after_navbar_dark");
+realtime_best.classList.add("realtime_best_dark");
+todays_hot.classList.add("todays_hot_dark");
+login_container.classList.add("login_container_dark");
+function backgroundDark() {
+    document.body.style.backgroundColor = "rgb(57, 62, 70)";
+}
+function backgroundLight() {
+    document.body.style.backgroundColor = "rgb(198, 193, 185)";
+}
+themeChangeBtn.addEventListener("click", changeTheme);
+function changeTheme() {
     if (dark == true) {
-        document.body.style.backgroundColor = "rgb(57, 62, 70)";
         dark = false;
+        backgroundLight();
+        navbar.classList.remove("navbar_dark");
+        after_navbar.classList.remove("after_navbar_dark");
+        realtime_best.classList.remove("realtime_best_dark");
+        todays_hot.classList.remove("todays_hot_dark");
+        login_container.classList.remove("login_container_dark");
+        navbar.classList.add("navbar_light");
+        after_navbar.classList.add("after_navbar_light");
+        realtime_best.classList.add("realtime_best_light");
+        todays_hot.classList.add("todays_hot_light");
+        login_container.classList.add("login_container_light");
     }
     else if (dark == false) {
         dark = true;
-        document.body.style.backgroundColor = "rgb(198, 193, 185)";
+        backgroundDark();
+        navbar.classList.remove("navbar_light");
+        after_navbar.classList.remove("after_navbar_light");
+        realtime_best.classList.remove("realtime_best_light");
+        todays_hot.classList.remove("todays_hot_light");
+        login_container.classList.remove("login_container_light");
+        navbar.classList.add("navbar_dark");
+        after_navbar.classList.add("after_navbar_dark");
+        realtime_best.classList.add("realtime_best_dark");
+        todays_hot.classList.add("todays_hot_dark");
+        login_container.classList.add("login_container_dark");
     }
 }
--- index/index.ts
+++ index/index.ts
@@ -1,17 +1,72 @@
-const colorChangeBtn: any = document.querySelector("#themeChangeBtn");
-const otherElements: any = document.querySelector("#mobgrid");
-const defaultTheme: string = "dark";
+// const otherElements: any = document.querySelector("#mobgrid");
+// const defaultTheme: string = "dark";
 
+// colorChangeBtn.addEventListener("click", clicked);
+
+// function clicked() {
+//     if (dark == true) {
+//         document.body.style.backgroundColor = "rgb(57, 62, 70)";
+//         dark = false;
+//     } else if (dark == false) {
+//         dark = true;
+//         document.body.style.backgroundColor = "rgb(198, 193, 185)";
+//     }
+// }
 let dark: boolean = true;
 
-colorChangeBtn.addEventListener("click", clicked);
+const themeChangeBtn: any = document.querySelector("#themeChangeBtn");
+const navbar: any = document.querySelector("#navbar");
+const after_navbar: any = document.querySelector("#after_navbar");
+const realtime_best: any = document.querySelector("#realtime_best");
+const todays_hot: any = document.querySelector("#todays_hot");
+const login_container: any = document.querySelector("#login_container");
+const mobgrid: any = document.querySelector("#mobgrid");
+backgroundDark();
 
-function clicked() {
+backgroundLight();
+navbar.classList.add("navbar_dark");
+after_navbar.classList.add("after_navbar_dark");
+realtime_best.classList.add("realtime_best_dark");
+todays_hot.classList.add("todays_hot_dark");
+login_container.classList.add("login_container_dark");
+
+function backgroundDark(): void {
+    document.body.style.backgroundColor = "rgb(57, 62, 70)";
+}
+function backgroundLight(): void {
+    document.body.style.backgroundColor = "rgb(198, 193, 185)";
+}
+
+themeChangeBtn.addEventListener("click", changeTheme);
+
+function changeTheme(): void {
     if (dark == true) {
-        document.body.style.backgroundColor = "rgb(57, 62, 70)";
         dark = false;
+        backgroundLight();
+        navbar.classList.remove("navbar_dark");
+        after_navbar.classList.remove("after_navbar_dark");
+        realtime_best.classList.remove("realtime_best_dark");
+        todays_hot.classList.remove("todays_hot_dark");
+        login_container.classList.remove("login_container_dark");
+
+        navbar.classList.add("navbar_light");
+        after_navbar.classList.add("after_navbar_light");
+        realtime_best.classList.add("realtime_best_light");
+        todays_hot.classList.add("todays_hot_light");
+        login_container.classList.add("login_container_light");
     } else if (dark == false) {
         dark = true;
-        document.body.style.backgroundColor = "rgb(198, 193, 185)";
+        backgroundDark();
+        navbar.classList.remove("navbar_light");
+        after_navbar.classList.remove("after_navbar_light");
+        realtime_best.classList.remove("realtime_best_light");
+        todays_hot.classList.remove("todays_hot_light");
+        login_container.classList.remove("login_container_light");
+
+        navbar.classList.add("navbar_dark");
+        after_navbar.classList.add("after_navbar_dark");
+        realtime_best.classList.add("realtime_best_dark");
+        todays_hot.classList.add("todays_hot_dark");
+        login_container.classList.add("login_container_dark");
     }
 }
