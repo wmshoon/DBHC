28e11f5fbe809f0e21509c43d26a841233d9583a
wmshoon <97453378+wmshoon@users.noreply.github.com>
2022-05-26 15:42:23 +0900

    good

--- index/index.js
+++ index/index.js
@@ -1,15 +1,4 @@
-"use strict";
-exports.__esModule = true;
-var themeChangeBtn = document.getElementById("themeChangeBtn");
-var lightTheme = false;
-themeChangeBtn.addEventListener("click", changeTheme);
-function changeTheme() {
-    if (lightTheme == true) {
-        alert("dark mode is enabled");
-        lightTheme = false;
-    }
-    else if (lightTheme == false) {
-        alert("dark mode is disabled");
-        lightTheme = true;
-    }
+function greeter(person) {
+    return "hello, " + person;
 }
+greeter(1);
--- index/index.ts
+++ index/index.ts
@@ -1,16 +1,5 @@
-export {};
-
-var themeChangeBtn = document.getElementById("themeChangeBtn");
-var lightTheme: boolean = false;
-
-themeChangeBtn.addEventListener("click", changeTheme);
-
-function changeTheme() {
-    if (lightTheme == true) {
-        alert("dark mode is enabled");
-        lightTheme = false;
-    } else if (lightTheme == false) {
-        alert("dark mode is disabled");
-        lightTheme = true;
-    }
+function greeter(person: string) {
+    return "hello, " + person;
 }
+
+greeter(1);
