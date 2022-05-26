cf38ced5c29a2bcfe30979486ee61caf5d20cfe0
wmshoon <97453378+wmshoon@users.noreply.github.com>
2022-05-26 20:20:22 +0900

    fuck you

--- index/index.js
+++ index/index.js
@@ -1,4 +1,15 @@
-function greeter(person) {
-    return "hello, " + person;
+var colorChangeBtn = document.querySelector("#themeChangeBtn");
+var otherElements = document.querySelector("#mobgrid");
+var defaultTheme = "dark";
+var dark = true;
+colorChangeBtn.addEventListener("click", clicked);
+function clicked() {
+    if (dark == true) {
+        document.body.style.backgroundColor = "rgb(57, 62, 70)";
+        dark = false;
+    }
+    else if (dark == false) {
+        dark = true;
+        document.body.style.backgroundColor = "rgb(198, 193, 185)";
+    }
 }
-greeter(1);
--- index/index.ts
+++ index/index.ts
@@ -1,5 +1,17 @@
-function greeter(person: string) {
-    return "hello, " + person;
-}
+const colorChangeBtn: any = document.querySelector("#themeChangeBtn");
+const otherElements: any = document.querySelector("#mobgrid");
+const defaultTheme: string = "dark";
 
-greeter(1);
+let dark: boolean = true;
+
+colorChangeBtn.addEventListener("click", clicked);
+
+function clicked() {
+    if (dark == true) {
+        document.body.style.backgroundColor = "rgb(57, 62, 70)";
+        dark = false;
+    } else if (dark == false) {
+        dark = true;
+        document.body.style.backgroundColor = "rgb(198, 193, 185)";
+    }
+}
